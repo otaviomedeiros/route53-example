ed66f535b9a14201082ad82da42d5e6be5dce88b
Otavio Medeiros <otaviorm@gmail.com>
2021-03-29 22:07:35 -0300

    Add route table and internet gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,7 +13,28 @@ resource "aws_vpc" "main_vpc" {
   instance_tenancy = "default"
 
   tags = {
-    Name = "Route 53 example VPC"
+    Name = "Route 53 example - VPC"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Route 53 example - Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Route 53 example - Public route table"
   }
 }
 
@@ -25,6 +46,16 @@ resource "aws_subnet" "public_subnet" {
   tags = {
     Name = "Route 53 example VPC - Public subnet"
   }
+}
+
+resource "aws_route_table_association" "public_rt_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.main_route_table.id
+}
+
+resource "aws_main_route_table_association" "main_rt_association" {
+  vpc_id         = aws_vpc.main_vpc.id
+  route_table_id = aws_route_table.main_route_table.id
 }
 
 output "vpc_id" {
