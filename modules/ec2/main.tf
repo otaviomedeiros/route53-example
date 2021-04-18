08e0ceabe5ab4dc097f38a34419aa48e13b63203
Otavio Medeiros <otaviorm@gmail.com>
2021-04-18 11:07:15 -0300

    Refactoring

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,59 +2,47 @@ variable "ami" {
   type = string
 }
 
-variable "hosted_zone_id" {
-  type = string
+variable "dns" {
+  type = object({
+    hosted_zone_id = string
+    sub_domain = string
+    geolocation_continent_code = string
+  })
 }
 
-variable "sub_domain" {
-  type = string
-}
-
-variable "availability_zone" {
-  type = string
-}
-
-variable "public_subnet_id" {
-  type = string
+variable "vpc" {
+  type = object({
+    availability_zone = string
+    public_subnet_id = string
+    security_group_id = string
+  })
 }
 
 variable "ssh_key_pair_name" {
   type = string
 }
 
-variable "security_group_id" {
-  type = string
-}
-
-variable "geolocation_continent_code" {
-  type = string
-}
-
-variable "nginx_file_content" {
-  type = string
-}
-
 resource "aws_instance" "ec2-instance" {
   ami                         = var.ami
   instance_type               = "t3.micro"
-  availability_zone           = var.availability_zone
+  availability_zone           = var.vpc.availability_zone
   associate_public_ip_address = true
   key_name                    = var.ssh_key_pair_name
-  vpc_security_group_ids      = [var.security_group_id]
-  subnet_id                   = var.public_subnet_id
-  user_data                   = templatefile("${path.module}/user_data.tpl", { nginx_file_content = var.nginx_file_content })
+  vpc_security_group_ids      = [var.vpc.security_group_id]
+  subnet_id                   = var.vpc.public_subnet_id
+  user_data                   = templatefile("${path.module}/user_data.tpl", { nginx_file_content = var.dns.geolocation_continent_code })
 }
 
 resource "aws_route53_record" "geo_dns_record" {
-  zone_id = var.hosted_zone_id
-  name    = "geo.${var.sub_domain}"
+  zone_id = var.dns.hosted_zone_id
+  name    = "geo.${var.dns.sub_domain}"
   type    = "A"
   ttl     = "60"
-  set_identifier = var.geolocation_continent_code
+  set_identifier = var.dns.geolocation_continent_code
   records = [aws_instance.ec2-instance.public_ip]
 
   geolocation_routing_policy {
-    continent = var.geolocation_continent_code
+    continent = var.dns.geolocation_continent_code
   }
 }
 
